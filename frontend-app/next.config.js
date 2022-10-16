fb7e8944a6d82f22242640f637fe604475b928c7
Meawhung <meawhung@gmail.com>
2022-10-16 16:42:49 +0900

    redirect index to shopLIst

--- frontend-app/next.config.js
+++ frontend-app/next.config.js
@@ -5,11 +5,20 @@ const nextConfig = {
   // deviceSizes: [320, 420, 768, 1024, 1200],
   // loader: "default",
   // domains: ["res.cloudinary.com"],
-}
+};
 module.exports = {
   images: {
-    domains: ['localhost'],
+    domains: ["localhost"],
   },
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/shopList",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 // module.exports = nextConfig
