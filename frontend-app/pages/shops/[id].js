e72aef4b51048986e197eb6b2ca035214337a351
spksoft <spkrsk.37@gmail.com>
2022-11-05 15:23:35 +0700

    refactor(frontend): splited container and presenter on shop[id] page

--- frontend-app/pages/shops/[id].js
+++ frontend-app/pages/shops/[id].js
@@ -4,7 +4,37 @@ import axios from "axios";
 import Modal from "../../components/modal/modal";
 import Review from "../../components/review";
 
-const ShopByIDPage = ({ id, shop, reviews, reviewTags }) => {
+const ShopPresenter = ({ shop, reviews, reviewTags }) => {
+  const { id } = shop;
+  const { name, address_detail, latitude, longitude, shop_images } = shop.attributes;
+
+  // prepare shop image
+  let url = "";
+  if (shop_images.data !== null) {
+    const name = shop_images.data[0].attributes.hash;
+    url = "http://localhost:1337/uploads/" + name + ".jpg";
+  }
+
+  return (
+    <>
+      <Link href="/shops"> BACK</Link>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>{name}</div>
+      <div>{address_detail}</div>
+      <div>{latitude}</div>
+      <div>{longitude}</div>
+      <div>{longitude}</div>
+      <Image src={url} alt="None" width={100} height={100} />
+      {reviews.map((review, index) => (
+        <Review key={index} review={review} />
+      ))}
+
+      <Modal shopId={id} reviewTags={reviewTags} />
+    </>
+  )
+}
+
+const Page = ({ shop, reviews, reviewTags }) => {
   let url = "";
   if (shop.attributes.shop_images.data !== null) {
     const name = shop.attributes.shop_images.data[0].attributes.hash;
@@ -12,32 +42,24 @@ const ShopByIDPage = ({ id, shop, reviews, reviewTags }) => {
   }
   return (
     <>
-      <Link href="/shopList"> BACK</Link>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>{shop.attributes.name}</div>
-      <div>{shop.attributes.address_detail}</div>
-      <div>{shop.attributes.latitude}</div>
-      <div>{shop.attributes.longitude}</div>
-      <div>{shop.attributes.longitude}</div>
-      <Image src={url} alt="None" width={100} height={100} />
-      {reviews.map((review, index) => (
-        <Review key={index} review={review} />
-      ))}
-
-      <Modal shopId={shop.id} reviewTags={reviewTags} />
+      <ShopPresenter
+        shop={shop}
+        reviews={reviews}
+        reviewTags={reviewTags}
+      />
     </>
   );
 };
 
-ShopByIDPage.getInitialProps = async (context) => {
-  const id = context.query.id;
+Page.getInitialProps = async (context) => {
+  const shopId = context.query.id;
   const shopRes = await axios.get(
-    `http://localhost:1337/api/Shops/${id}?populate=*`
+    `http://localhost:1337/api/Shops/${shopId}?populate=*`
   );
   const shop = shopRes.data.data;
 
   const reviewRes = await axios.get(
-    `http://localhost:1337/api/reviews?filters[shopId]=${id}`
+    `http://localhost:1337/api/reviews?filters[shopId]=${shopId}`
   );
   const reviews = reviewRes.data.data;
 
@@ -45,11 +67,10 @@ ShopByIDPage.getInitialProps = async (context) => {
   const reviewTags = reviewTagRes.data.data;
 
   return {
-    id,
     shop,
     reviews,
     reviewTags,
   };
 };
 
-export default ShopByIDPage;
+export default Page;
