ea799af1f656313b7656f4abb233c9a527b17c3a
spksoft <spkrsk.37@gmail.com>
2022-11-05 15:40:54 +0700

    chore(frontend): read config from env

---
+++ frontend-app/config/index.js
@@ -0,0 +1,7 @@
+const config =  {
+  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+}
+
+if (!config.apiBaseUrl) throw new Error('NEXT_PUBLIC_API_BASE_URL is not defined')
+
+export default config
