794813262ba521fc99a3b343ba1fb8e8d0383577
Pom <passavich.lim@gmail.com>
2022-11-13 15:05:42 +0900

    test1

--- frontend-app/pages/shops/index.js
+++ frontend-app/pages/shops/index.js
@@ -1,30 +1,36 @@
-import axios from "axios";
-import { useCallback } from "react";
+import axios from 'axios';
+import { useState, useCallback } from 'react';
+import SearchBox from '@/components/SearchBox';
 
 const ShopsPage = ({ shops, error }) => {
   if (error) {
     return <div>An error occured: {error.message}</div>;
   }
+  const [inputText, setinputText] = useState('');
   return (
-    <ul>
-      {shops.data.map((shop) => {
-        const id = shop.id;
-        const url = `/shop/${id}`;
-        return (
-          <div key={id}>
-            <a href={url}> {shop.attributes.name}</a>
-            <h4>{shop.attributes.address_detail}</h4>
-            <h4>latitude : {shop.attributes.latitude}</h4>
-            <h4>longtitude : {shop.attributes.longitude}</h4>
-          </div>
-        );
-      })}
-    </ul>
+    <div>
+      <SearchBox searchText={inputText} />
+      <ul>
+        {/* <p>ddd {inputSearch}</p> */}
+        {shops.data.map((shop) => {
+          const id = shop.id;
+          const url = `/shop/${id}`;
+          return (
+            <div key={id}>
+              <a href={url}> {shop.attributes.name}</a>
+              <h4>{shop.attributes.address_detail}</h4>
+              <h4>latitude : {shop.attributes.latitude}</h4>
+              <h4>longtitude : {shop.attributes.longitude}</h4>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 ShopsPage.getInitialProps = async (ctx) => {
   try {
-    const res = await axios.get("http://localhost:1337/api/Shops/?populate=*");
+    const res = await axios.get('http://localhost:1337/api/Shops/?populate=*');
     const shops = res.data;
     return { shops };
   } catch (error) {
